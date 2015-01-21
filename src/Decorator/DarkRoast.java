3fca4b0152f6215912e426779318f2a6b8fcbf65
Vinai Arunachalam <viarunachalam@paypal.com>
2015-01-21 12:39:22 -0800

    Added initial version

---
+++ src/Decorator/DarkRoast.java
@@ -0,0 +1,13 @@
+package Decorator;
+
+public class DarkRoast extends Beverage {
+	
+	public DarkRoast() {
+		description = "Dark Roast Coffee";
+	}
+
+	public double cost() {
+		return .99;
+	}
+
+}
---
+++ src/Decorator/HouseBlend.java
@@ -0,0 +1,12 @@
+package Decorator;
+
+public class HouseBlend extends Beverage {
+	
+	public HouseBlend() {
+		description = "Hoouse Blend Coffee Coffee";
+	}
+	
+	public double cost() {
+		return .89;
+	}
+}
